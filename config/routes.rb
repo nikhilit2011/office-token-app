e54568f934a590b893218247c427715f818a101d
NIKHIL <nikhil@NIKHILs-MacBook-Air.local>
2025-05-06 01:19:17 +0530

    commit with updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,41 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :tokens, only: [:new, :create]
+  # Token generation (for operator)
+  resources :tokens, only: [:new, :create] do
+    member do
+      get :print
+      patch :update_status
+    end
 
-  authenticated :user do
-    root to: "tokens#new", as: :authenticated_root
+    collection do
+      get :all_tokens
+      get :live_tokens
+      get :refresh_live_tokens
+      get :public_display
+      get :refresh_in_progress
+    end
   end
-  
-  get "counter_dashboard", to: "tokens#counter_dashboard"
-  patch "tokens/:id/update_status", to: "tokens#update_status", as: :update_token_status
-  
-  get "public_display", to: "tokens#public_display"
-  
-  get "tokens/:id/print", to: "tokens#print", as: :print_token
-  
-  get 'tokens/refresh_in_progress', to: 'tokens#refresh_in_progress'
-  
-  get "tokens/refresh_counter_dashboard", to: "tokens#refresh_counter_dashboard"
-  
-  
-  
+
+  # Legacy and renamed routes for clarity
+  get "counter_dashboard", to: redirect("tokens/all_tokens") # old name redirect
+
+  # Admin routes
   namespace :admin do
     resources :users, except: [:show]
   end
-  
+
+  # Authenticated user root (e.g., token operator lands on generate token)
+  authenticated :user do
+    root to: "tokens#new", as: :authenticated_root
+  end
+
+  # Devise logout
   devise_scope :user do
     get "/logout", to: "devise/sessions#destroy", as: :custom_logout
   end
 
+  # Default unauthenticated root
   devise_scope :user do
     unauthenticated do
       root to: "devise/sessions#new", as: :unauthenticated_root
